0079251e137f64b560e8247fef9c834d75a11ba6
Alex <103459735+torn80beta@users.noreply.github.com>
2024-04-08 20:54:33 +0300

    about, contacts, footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,41 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="flex flex-col gap-4 max-w-md mx-auto mt-4 text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, beatae sit corporis incidunt quaerat culpa optio
+            quis, eveniet odio perspiciatis, nostrum fugiat. Atque libero,
+            incidunt accusamus sapiente eius possimus adipisci.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, beatae sit corporis incidunt quaerat culpa optio
+            quis, eveniet odio perspiciatis, nostrum fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, beatae sit corporis incidunt quaerat culpa optio
+            quis.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a href="tel:+380677381212" className="text-4xl text-gray-500">
+            +38 067 738-12-12
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All rights reserved
+      </footer>
     </>
   );
 }
