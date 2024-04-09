3e5bead5308daea10f79e573d9ba9ea7060255de
Alex <103459735+torn80beta@users.noreply.github.com>
2024-04-09 20:23:42 +0300

    user registration

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import User from "../../models/User";
 import mongoose from "mongoose";
 
@@ -5,7 +6,15 @@ export async function POST(req) {
   const body = await req.json();
 
   await mongoose.connect(process.env.MONGO_URL);
-  const createdUser = await User.create(body);
 
-  return Response.json(createdUser);
+  try {
+    const createdUser = await User.create(body);
+    return Response.json(createdUser);
+  } catch (error) {
+    console.log(error.message);
+    return NextResponse.json(
+      { status: 400, message: error.message },
+      { status: 400 }
+    );
+  }
 }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -8,7 +8,9 @@ const UserSchema = new Schema(
       required: true,
       validate: (pass) => {
         if (!pass?.length || pass.length < 8) {
-          throw new Error("Password must be at least 8 characters long");
+          return Error({
+            message: "Password must be at least 8 characters long",
+          });
         }
       },
     },
