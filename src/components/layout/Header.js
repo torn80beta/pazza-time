1d5fcca51dce21d05412ec8b88258f90902abe31
Alex <103459735+torn80beta@users.noreply.github.com>
2024-04-09 15:40:41 +0300

    register page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,19 +3,22 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between items-center">
-      <Link className="text-primary font-semibold text-2xl" href="">
-        PIZZA TIME
-      </Link>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link href={""}>Home</Link>
+        <Link className="text-primary font-semibold text-2xl" href="/">
+          PIZZA TIME
+        </Link>
+        <Link href={"/"}>Home</Link>
         <Link href={""}>Menu</Link>
         <Link href={""}>About</Link>
         <Link href={""}>Contact</Link>
+      </nav>
+      <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        <Link href={"/login"}>Login</Link>
         <Link
           className="bg-primary rounded-full text-white px-8 py-2"
-          href={""}
+          href={"/register"}
         >
-          Login
+          Register
         </Link>
       </nav>
     </header>
